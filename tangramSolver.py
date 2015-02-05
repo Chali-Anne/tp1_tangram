605ddc65146e6d17dcac08f7358b42cb08bfc180
Chali-Anne <chali.anne.lauzon@gmail.com>
2015-02-04 22:39:23 -0500

    Juste les mêmes trucs que Tile en fait.

--- tangramSolver.py
+++ tangramSolver.py
@@ -8,6 +8,8 @@
 
 from astar_search import *
 from tangram import *
+from node import *
+from state import *
 
 class TangramSolver(object):
     def __init__(self, grid, pieces):
@@ -15,3 +17,11 @@ class TangramSolver(object):
         self.availablePieces = []
         for x in pieces:
             self.availablePieces.append(Tangram(x))
+        self.counter = 0        # The number of choices
+
+    # To check if two states are the same, we compare the puzzles' completion
+    def equals(self, state):
+        return self.puzzle == state.puzzle
+
+    # Equivalent to printing, but specific to a state
+    def show(self):
