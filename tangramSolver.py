e5ed935b060d086466f9d695be777c9daea80d7f
Chali-Anne <chali.anne.lauzon@gmail.com>
2015-02-04 21:22:50 -0500

    Début du solver.

--- tangramSolver.py
+++ tangramSolver.py
@@ -1,1 +1,17 @@
-__author__ = 'Chali'
+# -*- coding: utf-8 -*-
+#
+#  Class TangramSolver
+# Author: Chali-Anne Lauzon
+#         École Polytechnique de Montréal
+#
+# This class solves a tangram puzzle using a tree search
+
+from astar_search import *
+from tangram import *
+
+class TangramSolver(object):
+    def __init__(self, grid, pieces):
+        self.puzzle = grid      # The tangram puzzle
+        self.availablePieces = []
+        for x in pieces:
+            self.availablePieces.append(Tangram(x))
