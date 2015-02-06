786b806069bc77f7c721895b9bace127ea9b94ff
Chali-Anne <chali.anne.lauzon@gmail.com>
2015-02-05 21:44:11 -0500

    Hey je sais toujours pas comment resoudre le probleme...

--- tangramSolver.py
+++ tangramSolver.py
@@ -18,6 +18,7 @@ class TangramSolver(object):
         for x in pieces:
             self.availablePieces.append(Tangram(x))
         self.counter = 0        # The number of choices
+        self.emptyCells = 0     # The number of cells to fill
 
     # To check if two states are the same, we compare the puzzles' completion
     def equals(self, state):
@@ -25,3 +26,28 @@ class TangramSolver(object):
 
     # Equivalent to printing, but specific to a state
     def show(self):
+        for row in self.puzzle:
+            for cell in row:
+                if cell == -1:
+                    print ' '       # Empty spaces remain empty
+                else:
+                    print 'Y'       # Eventually print the piece's number
+
+    # Defines the cost of the state
+    def cost(self, action):
+        return 1
+
+    # Defines the goal conditions
+    def goal(self):
+        # If there are no more available pieces and there are no more empty spaces.
+        if not self.availablePieces and self.emptyCells == 0:
+            return True
+        return False
+
+    # Defines the effects of the actions
+    def executeAction(self, (action,x,y)):
+        self.counter += 1
+
+    # Defines the possible actions depending on the situation
+    def possibleActions(self):
+        actions = []
